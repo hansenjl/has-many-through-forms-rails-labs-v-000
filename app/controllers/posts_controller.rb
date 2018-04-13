ebcc20bf39b465da44ac81bcaf72e282e7d60257
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-04-13 00:53:39 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build 
   end
 
   def create
@@ -20,6 +21,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, categories_attributes[:name])
   end
 end
